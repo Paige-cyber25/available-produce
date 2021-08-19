fb1e0c2bb1eac27da3d404a2320bec70d6d963bb
Patience <patience@ext.kobo360.com>
2021-08-19 10:35:04 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import Products from './component/Products';
 import classes from './component/Product.module.css'
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div className={classes.app}>
+      <Navbar />
       <Products />
     </div>
   );
